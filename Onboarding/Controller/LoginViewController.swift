96b57643819382c5aefb39168dfac32c1315df6a
atleastzero <megan.obryan52@gmail.com>
2020-02-18 10:36:41 -0800

    Got rid of storyboards.

---
+++ Onboarding/Controller/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Onboarding
+//
+//  Created by Megan OBryan on 2/18/20.
+//  Copyright © 2020 Makeschool. All rights reserved.
+//
+
+import Foundation
--- Onboarding/Controller/OnboardingViewController.swift
+++ Onboarding/Controller/OnboardingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 
     var scrollView: UIScrollView!
     var container: UIStackView!
@@ -53,14 +53,6 @@ class ViewController: UIViewController {
         container.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
         container.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
         container.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
-        
-//        selfieView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-//        surveyView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-//        identityView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-        
-//        selfieView.backgroundColor = .cyan
-//        surveyView.backgroundColor = .brown
-//        identityView.backgroundColor = .gray
     }
     
     func setViews(){
@@ -95,85 +87,9 @@ class ViewController: UIViewController {
         pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         pageControl.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
-    
-//    func createView(currentView: UIView, imageName: String, text: String, isLastPage: Bool, color: UIColor) {
-//
-//        let stackView = UIStackView()
-//        stackView.axis = .vertical
-//        stackView.spacing = 20
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        stackView.distribution = .fill
-//
-//        currentView.addSubview(stackView)
-//
-//        stackView.widthAnchor.constraint(equalTo: currentView.layoutMarginsGuide.widthAnchor, multiplier: 0.65).isActive = true
-//        stackView.heightAnchor.constraint(equalTo: currentView.layoutMarginsGuide.heightAnchor, multiplier: 0.5).isActive = true
-//        stackView.centerXAnchor.constraint(equalTo: currentView.layoutMarginsGuide.centerXAnchor).isActive = true
-//        stackView.centerYAnchor.constraint(equalTo: currentView.layoutMarginsGuide.centerYAnchor).isActive = true
-//
-//        let imageView = UIImageView(image: UIImage(named: imageName))
-//        imageView.contentMode = .scaleAspectFit
-//        stackView.addArrangedSubview(imageView)
-//
-//        imageView.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 0.6).isActive = true
-//
-//        let message = UILabel()
-//        message.numberOfLines = 0
-//        message.textAlignment = .center
-//        message.text = text
-//        message.textColor = .purple
-//        message.font = UIFont(name: "American Typewriter", size: 20)
-//
-//        currentView.backgroundColor = color
-//    }
-    
-//
-    
-//    override func loadView() {
-//        super.loadView()
-//
-//        view.addSubview(pageControl)
-//
-//        selfieLabel.text = "Upload some selfies..."
-//        surveyLabel.text = "Take a quick survey..."
-//        identityLabel.text = "Assume your new identity!"
-//
-//
-//        selfieLabel.font = selfieLabel.font.withSize(20)
-//        surveyLabel.font = surveyLabel.font.withSize(20)
-//        identityLabel.font = identityLabel.font.withSize(20)
-//
-//        selfieLabel.translatesAutoresizingMaskIntoConstraints = false
-//        surveyLabel.translatesAutoresizingMaskIntoConstraints = false
-//        identityLabel.translatesAutoresizingMaskIntoConstraints = false
-//
-//        selfieView.addSubview(selfieLabel)
-//        surveyView.addSubview(surveyLabel)
-//        identityView.addSubview(identityLabel)
-//
-        
-//    }
-    
-        // Do any additional setup after loading the view.
-        
-//        selfieImage.image = UIImage(named: "selfie_image")
-//        surveyImage.image = UIImage(named: "survey_image")
-//        identityImage.image = UIImage(named: "identity_image")
-//
-//        selfieView.addSubview(selfieImage)
-//        surveyView.addSubview(surveyImage)
-//        identityView.addSubview(identityImage)
-//
-//        selfieView.bringSubviewToFront(selfieImage)
-//        surveyView.bringSubviewToFront(surveyImage)
-//        identityView.bringSubviewToFront(identityImage)
-//
-
-
-
 }
 
-extension ViewController: UIScrollViewDelegate{
+extension OnboardingViewController: UIScrollViewDelegate{
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
         pageControl.currentPage = Int(pageNumber)
